21b1ec9154363181ec315dd77984a33b8cf2d871
daigaro <daigaro@effrafax.org>
2011-10-21 19:25:59 +0200

    FrontController uses smarty
    The frontController is changed to test the smarty installation.

--- frontController.php
+++ frontController.php
@@ -1,20 +1,10 @@
-<html>
-	<head>
-		<title>FrontController Example</title>
-	</head>
-	<body>
-		<h1>FrontController Example</h1>
-		<p>
-			This is an example of a front controller.
-		</p>
-		<table>
-			<thead>
-				<tr><td>Parameter</td><td>Value</td></tr>
-			</thead>
-			<tbody>
-				<tr><td>part</td><td><?php echo $_GET["part"]; ?></td></tr>
-				<tr><td>section</td><td><?php echo $_GET["section"]; ?></td></tr>          
-			</tbody
-		</table>
-	</body>
-</html>
+<?php
+	require('smarty/lib/Smarty.class.php');
+	$smarty = new Smarty();
+	$smarty->setTemplateDir('smarty/templates');
+	$smarty->setCompileDir('smarty/templates_c');
+	$smarty->setCacheDir('smarty/cache');
+	$smarty->setConfigDir('smarty/configs');
+	
+	$smarty->testInstall();
+?>
